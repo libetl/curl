5a618ad171db0ca3b3db461d97ea70a0d7ce3656
lbenychou <lbenychou@egencia.com>
2018-05-20 13:12:33 +0200

    arguments table

--- src/test/java/org/toilelibre/libe/curl/ArgumentsBuilderGeneratorTest.java
+++ src/test/java/org/toilelibre/libe/curl/ArgumentsBuilderGeneratorTest.java
@@ -1,20 +1,29 @@
 package org.toilelibre.libe.curl;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.commons.cli.Option;
-import org.junit.Test;
-import org.toilelibre.libe.curl.Curl.CurlArgumentsBuilder;
-
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.NotFoundException;
+import org.apache.commons.cli.Option;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.toilelibre.libe.curl.Curl.CurlArgumentsBuilder;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
 
 public class ArgumentsBuilderGeneratorTest {
 
@@ -23,7 +32,7 @@ public class ArgumentsBuilderGeneratorTest {
     private static final List<String> DIGITS         = Arrays.asList ("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
 
     @Test
-    public void addOptionsToArgumentsBuilder () throws ClassNotFoundException, NotFoundException, CannotCompileException, IOException {
+    public void addOptionsToArgumentsBuilder () throws NotFoundException, CannotCompileException, IOException {
 
         final ClassPool pool = ClassPool.getDefault ();
         final CtClass argsBuilderClass = pool.get (CurlArgumentsBuilder.class.getName ());
@@ -47,6 +56,61 @@ public class ArgumentsBuilderGeneratorTest {
         argsBuilderClass.writeFile ("target/classes");
     }
 
+    @Test
+    public void addOptionsToReadmeMarkdown () throws IOException {
+        final int shortNamesMaxLength = Arguments.ALL_OPTIONS.getOptions().stream().map(Option::getOpt)
+                .map(String::length).max(Integer::compareTo).orElse(0);
+        final int longNamesMaxLength = Arguments.ALL_OPTIONS.getOptions().stream().map(Option::getLongOpt)
+                .map(String::length).max(Integer::compareTo).orElse(0);
+        final int descriptionMaxLength = Arguments.ALL_OPTIONS.getOptions().stream().map(Option::getDescription)
+                .map(String::length).max(Integer::compareTo).orElse(0);
+
+        StringBuilder tableBuilder = new StringBuilder();
+        tableBuilder.append("| Short Name ").append(IntStream.range(1, Math.max(0, shortNamesMaxLength - 9))
+                .mapToObj(i -> " ").collect(joining()));
+        tableBuilder.append("| Long Name ").append(IntStream.range(1, Math.max(0, longNamesMaxLength - 8))
+                .mapToObj(i -> " ").collect(joining()));
+        tableBuilder.append("| Argument Required ");
+        tableBuilder.append("| Description ").append(IntStream.range(0, Math.max(0, descriptionMaxLength - 11))
+                .mapToObj(i -> " ").collect(joining())).append("|\n");
+        tableBuilder.append("| ").append(IntStream.range(1, Math.max(0, shortNamesMaxLength) + 1)
+                .mapToObj(i -> "-").collect(joining())).append(' ');
+        tableBuilder.append("| ").append(IntStream.range(1, Math.max(0, longNamesMaxLength) + 1)
+                .mapToObj(i -> "-").collect(joining())).append(' ');
+        tableBuilder.append("| ----------------- ");
+        tableBuilder.append("| ").append(IntStream.range(1, Math.max(0, descriptionMaxLength) + 1)
+                .mapToObj(i -> "-").collect(joining())).append(" |\n");
+
+        for (final Option option : Arguments.ALL_OPTIONS.getOptions ()) {
+            tableBuilder.append("| ").append(option.getOpt())
+                    .append(IntStream.range(1, Math.max(0, shortNamesMaxLength - option.getOpt().length() + 2))
+                    .mapToObj(i -> " ").collect(joining()));
+            tableBuilder.append("| ").append(option.getLongOpt())
+                    .append(IntStream.range(1, Math.max(0, longNamesMaxLength - option.getLongOpt().length() + 2))
+                            .mapToObj(i -> " ").collect(joining()));
+            tableBuilder.append("| ").append(option.hasArg())
+                    .append(IntStream.range(1, Math.max(0, 19 - (option.hasArg() ? 4 : 5)))
+                            .mapToObj(i -> " ").collect(joining()));
+            tableBuilder.append("| ").append(option.getDescription().replace('|', ','))
+                    .append(IntStream.range(1, Math.max(0, descriptionMaxLength - option.getDescription().length() + 2))
+                            .mapToObj(i -> " ").collect(joining())).append("|\n");
+        }
+        final File readme = stream(Objects.requireNonNull(new File(ArgumentsBuilderGeneratorTest.class
+                .getProtectionDomain().getCodeSource().getLocation().getFile()).getParentFile().getParentFile()
+                .listFiles())).filter(f -> "README.md".equalsIgnoreCase(f.getName()))
+                .findFirst().orElseThrow(() -> new FileNotFoundException("README.md"));
+        final String readmeContent = IOUtils.toString(new FileInputStream(readme));
+
+        final String newReadmeContent = Pattern.compile(
+                "Arguments supported \\(so far\\) :")
+                .splitAsStream(readmeContent).findFirst()
+                .orElse("") + "Arguments supported (so far) :\n\n" + tableBuilder.toString();
+
+        FileWriter writer = new FileWriter(readme);
+        writer.write(newReadmeContent);
+        writer.close();
+    }
+
     private CtMethod builderOptionMethod (final CtClass ctClass, final CtClass stringType, final String methodName, final String optName, final boolean hasArg) throws CannotCompileException {
         final CtMethod method = new CtMethod (ctClass, methodName, hasArg ? new CtClass [] { stringType } : new CtClass [0], ctClass);
         method.setBody ("{curlCommand.append (\"-" + (optName.length () == 1 ? "" : "-") + optName + " \"" + (hasArg ? " + $1 + \" \"" : "") + ");\n" + "    " + "return $0;}");
@@ -68,7 +132,7 @@ public class ArgumentsBuilderGeneratorTest {
         final Matcher matcher = ArgumentsBuilderGeneratorTest.DIGITS_PATTERN.matcher (lowerCased);
 
         while (matcher.find ()) {
-            final StringBuffer replacement = new StringBuffer ();
+            final StringBuilder replacement = new StringBuilder ();
             for (int i = 0; i < matcher.group (1).length (); i++) {
                 replacement.append (ArgumentsBuilderGeneratorTest.DIGITS.get (matcher.group (1).charAt (i) - '0'));
             }
