9e77f1ac2876fc9dfeb86696a3b80680cf2ec712
lionel benychou <lbenychou@egencia.com>
2019-04-08 19:07:08 +0200

    curl should not be async by default

--- src/main/java/org/toilelibre/libe/curl/Version.java
+++ src/main/java/org/toilelibre/libe/curl/Version.java
@@ -1,5 +1,5 @@
 package org.toilelibre.libe.curl;
 public final class Version {
  public static String VERSION = "0.0.29";
- public static String BUILD_TIME = "2019-04-08T17:02:23Z";
+ public static String BUILD_TIME = "2019-04-08T17:05:57Z";
 }
