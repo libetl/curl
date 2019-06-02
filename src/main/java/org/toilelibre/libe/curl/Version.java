b093624147c795b8edefe86c707545228ecfa9a3
libetl <libe4@free.fr>
2019-06-02 14:48:40 +0200

    upgrading jackson-databind

--- src/main/java/org/toilelibre/libe/curl/Version.java
+++ src/main/java/org/toilelibre/libe/curl/Version.java
@@ -1,5 +1,5 @@
 package org.toilelibre.libe.curl;
 public final class Version {
- public static String VERSION = "0.0.33-SNAPSHOT";
- public static String BUILD_TIME = "2019-06-02T11:26:31Z";
+ public static String VERSION = "0.0.34-SNAPSHOT";
+ public static String BUILD_TIME = "2019-06-02T12:48:00Z";
 }
