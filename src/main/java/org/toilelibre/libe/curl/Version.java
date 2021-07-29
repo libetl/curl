99ba69340b0fea1d66387c267e90f3aef905c81d
Lionel Benychou <lbenychou@egencia.com>
2021-07-29 17:38:16 +0200

    remove tomcat embed core

--- src/main/java/org/toilelibre/libe/curl/Version.java
+++ src/main/java/org/toilelibre/libe/curl/Version.java
@@ -1,6 +1,6 @@
 package org.toilelibre.libe.curl;
 
 final class Version {
-  static String NUMBER = "0.0.39-SNAPSHOT";
-  static String BUILD_TIME = "2020-10-31T11:02:08Z";
+ static String NUMBER = "0.0.40-SNAPSHOT";
+ static String BUILD_TIME = "2021-07-29T15:37:06Z";
 }
