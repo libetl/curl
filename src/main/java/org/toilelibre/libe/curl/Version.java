5c4d323c3ffd55203df22d3144d8bb42c071c51a
lionel benychou <lbenychou@egencia.com>
2023-08-08 20:12:40 +0200

    upgrade plugins versions

--- src/main/java/org/toilelibre/libe/curl/Version.java
+++ src/main/java/org/toilelibre/libe/curl/Version.java
@@ -1,6 +1,6 @@
 package org.toilelibre.libe.curl;
 
 final class Version {
- static String NUMBER = "0.0.41-SNAPSHOT";
- static String BUILD_TIME = "2023-08-08T17:50:00Z";
+ static String NUMBER = "0.0.41";
+ static String BUILD_TIME = "2023-08-08T17:55:12Z";
 }
