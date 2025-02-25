66d15568c829b3357bdfc1db4ad1ca8b0bd72e4a
lionel.benychou <lionel.benychou@amexgbt.com>
2025-02-25 17:06:42 +0100

    curl is not compatible with httpclient 5.4.1

--- src/main/java/org/toilelibre/libe/curl/Version.java
+++ src/main/java/org/toilelibre/libe/curl/Version.java
@@ -2,5 +2,5 @@ package org.toilelibre.libe.curl;
 
 final class Version {
  static String NUMBER = "0.0.44-SNAPSHOT";
- static String BUILD_TIME = "2024-07-30T13:17:52Z";
+ static String BUILD_TIME = "2025-02-25T16:04:48Z";
 }
