f3bac3de0d9e84608b973523648ba98996ec4d6c
LiBe <libe4@free.fr>
2025-02-25 22:26:58 +0100

    0.0.44 : having some problems with ossrh, switching to manual deployment

--- src/main/java/org/toilelibre/libe/curl/Version.java
+++ src/main/java/org/toilelibre/libe/curl/Version.java
@@ -1,6 +1,6 @@
 package org.toilelibre.libe.curl;
 
 final class Version {
- static String NUMBER = "0.0.44-SNAPSHOT";
- static String BUILD_TIME = "2025-02-25T20:50:36Z";
+ static String NUMBER = "0.0.44";
+ static String BUILD_TIME = "2025-02-25T21:21:37Z";
 }
