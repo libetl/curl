7c5f982dad20b1f4f85bf0d734a9b6031e844072
lionel.benychou <lionel.benychou@amexgbt.com>
2025-12-04 12:15:29 +0100

    fixing version number

--- src/main/java/org/toilelibre/libe/curl/Version.java
+++ src/main/java/org/toilelibre/libe/curl/Version.java
@@ -1,6 +1,6 @@
 package org.toilelibre.libe.curl;
 
 final class Version {
- static String NUMBER = "0.0.46";
- static String BUILD_TIME = "2025-12-04T11:09:14Z";
+ static String NUMBER = "0.0.45-SNAPSHOT";
+ static String BUILD_TIME = "2025-12-04T11:13:04Z";
 }
