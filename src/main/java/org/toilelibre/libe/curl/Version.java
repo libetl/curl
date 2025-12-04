f60af91e58a32795186d9d9b4e457e503aff9058
lionel.benychou <lionel.benychou@amexgbt.com>
2025-12-04 12:09:38 +0100

    upgrades

--- src/main/java/org/toilelibre/libe/curl/Version.java
+++ src/main/java/org/toilelibre/libe/curl/Version.java
@@ -2,5 +2,5 @@ package org.toilelibre.libe.curl;
 
 final class Version {
  static String NUMBER = "0.0.46";
- static String BUILD_TIME = "2025-12-04T11:05:23Z";
+ static String BUILD_TIME = "2025-12-04T11:09:14Z";
 }
